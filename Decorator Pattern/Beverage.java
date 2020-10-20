93cfc7079c65393192c5cb45faf30613b50b6e20
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2020-10-20 00:26:24 -0700

    Update Beverage.java

--- Decorator Pattern/Beverage.java
+++ Decorator Pattern/Beverage.java
@@ -9,52 +9,49 @@ public abstract class Beverage {
   public abstract double cost(); 
 }
 
+      public class Espresso extends Beverage {
+        public Espresso()    {  description = “Espresso”;}
+        public double cost() {  return 1.99;} 
+      }
+
+      public class Decaf extends Beverage { 
+          public Decaf() {...}
+           public double cost() {...}
+      }
+
+      public class HouseBlend extends Beverage { 
+          public HouseBlend() {...}
+           public double cost() { ...}
+      }
+
+
+
+
 // DECORATOR ABSTRACT -- [this doesn't have to be abstract in JAVA IO the decorator base case FilterInputStream is instantiable ] 
-public abstract class CondimentDecorator extends Beverage {    // IMP Decorator extends the base component so it has the same type 
+public abstract class CondimentDecorator extends Beverage {   
   public abstract String getDescription();
 }
 
-/************************************ COMPONENT - Concrete classes ******************/
-public class Espresso extends Beverage {
-  public Espresso()    {  description = “Espresso”;}
-  public double cost() {  return 1.99;} 
-}
+          public class Mocha extends CondimentDecorator { 
 
-public class Decaf extends Beverage { 
-    public Decaf() {...}
-     public double cost() {...}
-}
+            Beverage beverage;                                // IMP contians Beverage object [Composition]
+            public Mocha(Beverage beverage) {                 // IMP cannot instantiate without concrete Beverage class [Espresso, DarkRoast, Decaf..]
+              this.beverage = beverage;
+            }
+            public String getDescription() { return beverage.getDescription() + “, Mocha”; }  // how the recursion is used to decorate (add functionality)
+            public double cost() { return .20 + beverage.cost();}                             // how the recursion is used to decorate (add functionality)
+          }
 
-public class HouseBlend extends Beverage { 
-    public HouseBlend() {...}
-     public double cost() { ...}
-}
-/* *********************************** END COMPONENT - Concrete classes ******************/
+          public class Milk extends CondimentDecorator { 
 
+            Beverage beverage;                                // IMP contians Beverage object [Composition]
+            public Milk(Beverage beverage) {                  // IMP cannot instantiate without concrete Beverage class [Espresso, DarkRoast, Decaf..]
+              this.beverage = beverage;
+            }
+            public String getDescription() { return beverage.getDescription() + “, Milk”; }
+            public double cost() { return .20 + beverage.cost();} 
+          }
 
-
-
-/************************************ DECORATOR - Concrete classes ******************/
-public class Mocha extends CondimentDecorator { 
-
-  Beverage beverage;                                // IMP contians Beverage object [Composition]
-  public Mocha(Beverage beverage) {                 // IMP cannot instantiate without concrete Beverage class [Espresso, DarkRoast, Decaf..]
-    this.beverage = beverage;
-  }
-  public String getDescription() { return beverage.getDescription() + “, Mocha”; }  // how the recursion is used to decorate (add functionality)
-  public double cost() { return .20 + beverage.cost();}                             // how the recursion is used to decorate (add functionality)
-}
-
-public class Milk extends CondimentDecorator { 
-
-  Beverage beverage;                                // IMP contians Beverage object [Composition]
-  public Milk(Beverage beverage) {                  // IMP cannot instantiate without concrete Beverage class [Espresso, DarkRoast, Decaf..]
-    this.beverage = beverage;
-  }
-  public String getDescription() { return beverage.getDescription() + “, Milk”; }
-  public double cost() { return .20 + beverage.cost();} 
-}
-/************************************ END  DECORATOR - Concrete classes ******************/
 
 public class StarbuzzCoffee {
   public static void main(String args[]) { 

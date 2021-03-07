ce2f58dadaf07ddc84129b3e77ba485319a55c7c
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-03-06 22:27:11 -0800

    Update Criteria.java

--- Filter/PersonFilter/Criteria.java
+++ Filter/PersonFilter/Criteria.java
@@ -3,3 +3,12 @@ import java.util.List;
 public interface Criteria {
    public List<Person> meetCriteria(List<Person> persons);
 }
+
+
+/*
+here actually you can have Generic Type so that any class can implement this Criteria
+*/
+
+public interface Criteria<T> {
+   public List<T> meetCriteria(List<T> list);
+}

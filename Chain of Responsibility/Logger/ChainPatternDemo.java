8e3f8937e8a0f866dea0f3892dec4622e183db1c
xXLogicNotFoundXx <40186122+xXLogicNotFoundXx@users.noreply.github.com>
2021-05-07 11:07:21 -0700

    Update ChainPatternDemo.java

--- Chain of Responsibility/Logger/ChainPatternDemo.java
+++ Chain of Responsibility/Logger/ChainPatternDemo.java
@@ -1,7 +1,7 @@
 public class ChainPatternDemo {
  
    // Nice that we have static factory method idiom for creating a chain.
-   // Simple Factory  
+   // Simple Factory / Static Factory
    private static AbstractLogger getChainOfLoggers(){
 
       AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
